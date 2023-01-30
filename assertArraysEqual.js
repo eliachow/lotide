48ee147207e98b8a4e402eea9fc7e3d78e2137e4
Elia Chow <eliacchow@gmail.com>
2023-01-29 17:01:12 -0800

    created assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,35 @@
+/*
+---- INSTRUCTIONS ----
+Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+
+---- PUESDOCODE ---- 
+const assertArrayEqual = fuction (array1, array2) {
+  if ( array1 !== array2 ) {
+    console.log(`🚫Assertion failed: ${array1} !== ${array2}`)
+  }
+  console.log(`✅Assertion passed: ${array1} === ${Array2}`)
+}
+
+*/
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed; ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+console.log(assertArraysEqual([1, 2, "3"], [1, 2, 3])); // => false
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(assertArraysEqual([5, 6, 7, 8], [5, 6, 7, 8]));
+console.log(assertArraysEqual([3, 2, 1], [1, 2, 3]));
