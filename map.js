5cc5b833ed9e4fc24d1bace5b23d8ef328c6ba0d
Elia Chow <eliacchow@gmail.com>
2023-02-05 11:30:28 -0800

    created map.js to find first index of items in an array

---
+++ map.js
@@ -0,0 +1,45 @@
+/*
+---- Implement Map ----
+https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m01w2/activities/227?workbook=5
+
+---- Function Definition ----
+Our map function will take in two arguments:
+
+An array to map
+A callback function
+The map function will return a new array based on the results of the callback function.
+
+*/
+
+// assertion test
+const eqArrays = function (arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed; ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
+
+console.log(assertArraysEqual(results1, ["g", "c", "t", "m", "t"]));
