173608fea5ef8ec8c046417f3a9e52f0f1da2530
eliachow <eliacchow@gmail.com>
2023-02-18 11:40:17 -0800

    refactored to implement mocha & chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,23 @@
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
+// const assertEqual = require('../assertEqual')
+// const tail = require('../tail')
 
 
-//test code
-assertEqual(tail([1,2,3]), 2)
-assertEqual(tail(["one", "two", "three", "four"]), 3)
+// //test code
+// assertEqual(tail([1,2,3]), 2)
+// assertEqual(tail(["one", "two", "three", "four"]), 3)
+
+
+//---- REFACTORING: IMPLEMENTING MOCHA & CHAI ----
+
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+    it("returns 2 for [1,2,3]", () => {
+        assert.strictEqual(tail([1,2,3]), 2);
+    });
+
+    it("returns 3 for [\"one\", \"two\", \"three\", \"four\"]", () => {
+        assert.strictEqual(tail(["one", "two", "three", "four"]),3);
+    });
+});
