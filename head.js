e58c27580a52f0d4823687e1cc9bb244eeb1f5ea
eliachow <eliacchow@gmail.com>
2023-02-20 18:51:23 -0800

    eslint head & headTest

--- head.js
+++ head.js
@@ -9,16 +9,15 @@
 
 
 //refactoring: importing assertEqual
-const assertEqual = require('./assertEqual')
+//const assertEqual = require('./assertEqual')
 
-const head = function (array) {
+const head = function(array) {
   return array[0];
 };
 
-
-module.exports = head
+module.exports = head;
 
 // //refactorings: moving to test/headTest.js
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([], undefined));
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([], undefined));
