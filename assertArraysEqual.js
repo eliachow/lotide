ba44f7d956ac3d03492cd0d1e6cd1672d51a3189
eliachow <eliacchow@gmail.com>
2023-02-20 19:30:29 -0800

    eslint assertArraysEqual & assertArraysEqualTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,7 +2,7 @@
 ---- INSTRUCTIONS ----
 Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
----- PUESDOCODE ---- 
+---- PUESDOCODE ----
 const assertArrayEqual = fuction (array1, array2) {
   if ( array1 !== array2 ) {
     console.log(`🚫Assertion failed: ${array1} !== ${array2}`)
@@ -12,16 +12,35 @@ const assertArrayEqual = fuction (array1, array2) {
 
 */
 
-const eqArrays = function (arrayOne, arrayTwo) {
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// ---- ORIGINAL ----
+// const eqArrays = function (arrayOne, arrayTwo) {
+//   for (let i = 0; i < arrayOne.length; i++) {
+//     if (arrayOne[i] !== arrayTwo[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-const assertArraysEqual = function (arr1, arr2) {
+// const assertArraysEqual = function (arr1, arr2) {
+//   if (eqArrays(arr1, arr2)) {
+//     console.log(`✅ Assertion Passed; ${arr1} === ${arr2}`);
+//   } else {
+//     console.log(`🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+//   }
+// };
+
+// console.log(assertArraysEqual([1, 2, "3"], [1, 2, 3])); // => false
+// console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+// console.log(assertArraysEqual([5, 6, 7, 8], [5, 6, 7, 8]));
+// console.log(assertArraysEqual([3, 2, 1], [1, 2, 3]));
+
+
+// ---- REFACTORING FOR MODULE TESTING ----
+
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅ Assertion Passed; ${arr1} === ${arr2}`);
   } else {
@@ -29,7 +48,4 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-console.log(assertArraysEqual([1, 2, "3"], [1, 2, 3])); // => false
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
-console.log(assertArraysEqual([5, 6, 7, 8], [5, 6, 7, 8]));
-console.log(assertArraysEqual([3, 2, 1], [1, 2, 3]));
+module.exports = assertArraysEqual;
