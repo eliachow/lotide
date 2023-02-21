415e23183aeb32098e41807deefcff9f0f2a67d3
eliachow <eliacchow@gmail.com>
2023-02-20 18:40:51 -0800

    updating format

--- middle.js
+++ middle.js
@@ -18,25 +18,58 @@ middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
 
 */
 
+//---- ORIGINAL ----
+
 // TEST/ASSERTION FUNCTIONS
-const eqArrays = function (arrayOne, arrayTwo) {
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = function (arrayOne, arrayTwo) {
+//   for (let i = 0; i < arrayOne.length; i++) {
+//     if (arrayOne[i] !== arrayTwo[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed; ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🚫 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+// const assertArraysEqual = function (arr1, arr2) {
+//   if (eqArrays(arr1, arr2)) {
+//     console.log(`✅ Assertion Passed; ${arr1} === ${arr2}`);
+//   } else {
+//     console.log(`🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+//   }
+// };
 
-// ACTUAL FUNCTION
+// //FUNCTION CODE
+// const middle = function (array) {
+//   const mid = Math.ceil(array.length / 2) - 1;
+//   //1-2 array
+//   if (array.length < 3) {
+//     return [];
+//   }
+//   //even array
+//   if (array.length % 2 === 0) {
+//     return [array[mid], array[mid + 1]];
+//   }
+//   //odd array
+//   if (array.length % 2 !== 0) {
+//     return [array[mid]];
+//   }
+// };
+
+// // TEST CODE
+// assertArraysEqual(middle([1]), []); // => []
+// assertArraysEqual(middle([1, 2]), []); // => []
+
+// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+// assertArraysEqual(middle([2, 4, 6, 8, 10, 12, 14]), [8, 10]);
+
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+// assertArraysEqual(middle([2, 4, 6, 8, 10, 12, 14, 16]), [8, 10]); // => [8, 10]
+
+//---- REFACTORING TO FOR MODULE TESTING ----
+
+//FUNCTION CODE
 const middle = function (array) {
   const mid = Math.ceil(array.length / 2) - 1;
   //1-2 array
@@ -53,14 +86,6 @@ const middle = function (array) {
   }
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
 
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([2, 4, 6, 8, 10, 12, 14]), [8, 10]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-assertArraysEqual(middle([2, 4, 6, 8, 10, 12, 14, 16]), [8, 10]); // => [8, 10]
+//export function
+module.exports = middle
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,5 @@
-// //import functions
 // const assertArraysEqual = require('../assertArraysEqual');
 // const middle = require('../middle');
-
-
 
 
 // // TEST CODE
@@ -32,11 +29,11 @@ describe("#middle", () => {
     })
 
     it("returns [] for [1, 2]", () => {
-        assert.deepEqual(middle([1,2]), [])
+        assert.deepEqual(middle([1, 2]), [])
     })
 
     it("returns [2] for [1, 2, 3]", () => {
-      assert.deepEqual(middle([1, 2, 3]), [2]);
+        assert.deepEqual(middle([1, 2, 3]), [2]);
     });
 
     it("returns [3] for [1, 2, 3, 4, 5]", () => {
@@ -46,4 +43,4 @@ describe("#middle", () => {
     it("returns [2, 3] for [1, 2, 3, 4]", () => {
         assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
     })
-  });
+});
